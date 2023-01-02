54265c48785f076a9f38558addb627372b0300de
jonathan bola <bolajonathan@yahoo.com>
2023-01-02 12:49:55 -0600

    relationships & validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,5 @@
 class Recipe < ApplicationRecord
+    belongs_to :user
+    belongs_to :cuisine
+    has_many :favorites, through: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    validates :username, presence: true
+    has_many :recipes
+    has_many :cuisines, through: :recipes
+    has_many :favorites
+    has_secure_password
 end
