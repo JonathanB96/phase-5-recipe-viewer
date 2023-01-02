b4fc79ed583c1d74c31a7e4b7b29a7a2578f8187
jonathan bola <bolajonathan@yahoo.com>
2023-01-02 13:43:49 -0600

    creating components

---
+++ client/src/components/App.js
@@ -0,0 +1,19 @@
+import Home from './Home';
+import NavBar from './NavBar';
+import Signup from './Signup';
+import NewRecipeForm from './NewRecipeForm';
+import Login from './Login';
+import RecipeList from './RecipeList';
+import Tips from './Tips';
+import UserRecipes from './UserRecipes';
+
+
+function App() {
+  return (
+    <div className="App">
+      HELLO
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/NewRecipeForm.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NewRecipeForm() {
+  return (
+    <div>NewRecipeForm</div>
+  )
+}
