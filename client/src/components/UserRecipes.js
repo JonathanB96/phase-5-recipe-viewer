1ac06571d0e4cda6d44c6768464fbdfb1b881581
jonathan bola <bolajonathan@yahoo.com>
2023-01-02 20:05:51 -0600

    user favorites list

--- client/src/components/UserRecipes.js
+++ client/src/components/UserRecipes.js
@@ -1,7 +1,68 @@
-import React from 'react'
+import React, {useContext, useEffect, useState} from 'react'
+import { UserContext } from './user'
 
 export default function UserRecipes() {
-  return (
-    <div>UserRecipes</div>
-  )
+  const { user, setUser } = useContext(UserContext);
+
+  useEffect(() => {
+    fetch(`/${user.id}/favorites`)
+      .then((r) => r.json())
+      .then((recipes)=>{
+        console.log(recipes)       
+      
+      });
+  }, []);
+  const [recipes, setRecipes] = useState([])
+  const [recipeList, setRecipeList]= useState([])
+  
+  useEffect(() => {
+    fetch("/favorites")
+      .then((r) => r.json())
+      .then((recipes)=>{
+        setRecipes(recipes)
+        setRecipeList(recipes)
+        console.log(recipes)
+      
+      });
+  }, []);
+
+  function deleteFavorite(e){
+    
+    let id = e.target.parentNode.getAttribute("title")
+    fetch(`/favorites/${id}`,{
+      method: "DELETE"
+    })
+    .then(r=>{
+      if(r.ok){
+        const newRecipeList = recipeList.filter((r)=>{
+          return r.id!= id          
+        })
+        setRecipes(newRecipeList)  
+        
+
+      }
+      })
+  }
+    
+    
+  return <>
+  
+  <div id="wrapper">
+    <h1>My recipes</h1>
+           <h2>
+            My Favorites
+           </h2>
+            {recipes.map((recipe)=>{
+              return <div key={recipe.id} title={recipe.id}>
+                  <h2>{recipe.recipe_name}</h2>
+                  <p>{recipe.recipe_steps}</p>
+                  <button className='myButton' onClick={deleteFavorite}>Delete</button>
+              </div>
+            })}
+           
+ </div>
+
+  
+</>
+   
 }
