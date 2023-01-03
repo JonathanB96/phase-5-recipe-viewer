4de5cb8c81cd2e53ee71e9aa65df29bf813f5b62
jonathan bola <bolajonathan@yahoo.com>
2023-01-02 20:00:13 -0600

    Card component

--- client/src/components/NewRecipeForm.js
+++ client/src/components/NewRecipeForm.js
@@ -1,7 +1,102 @@
-import React from 'react'
+import React,{useState, useContext} from 'react'
+import { Link } from "react-router-dom";
+import { UserContext } from './user'
+
 
 export default function NewRecipeForm() {
-  return (
-    <div>NewRecipeForm</div>
-  )
+  const [recipeName, setRecipeName] = useState("")
+  const [imgUrl, setImgUrl] = useState("")
+  const [steps, setSteps] = useState("")
+  const { user, setUser } = useContext(UserContext);
+  const [cuisine, setCuisine] = useState("")
+
+  const formData = {
+    name: "",
+    steps:"",
+    image_url:"",
+    user_id:"",
+    cuisine:""}
+
+
+  function handleName(e){
+    setRecipeName(e.target.value)
+
+  }
+  function handleUrl(e){
+    setImgUrl(e.target.value)
+
+  }
+  function handleSteps(e){
+    setSteps(e.target.value)
+
+  }
+
+  function handleCuisine(e){
+    setCuisine(e.target.value)
+
+  }
+  function handleSubmit(e){
+    e.preventDefault()
+
+      formData.name = recipeName
+      formData.image_url= imgUrl
+      formData.steps= steps
+      formData.user_id = user.id
+      formData.cuisine = cuisine
+
+      fetch(`/recipes`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"},
+      body: JSON.stringify(formData)
+    }).then((r)=>r.json())
+      .then((newReview)=>{
+        console.log(newReview)
+        setRecipeName("")
+        setImgUrl("")
+        setCuisine("")
+        setSteps("")
+      })  
+    
+    }
+
+          
+    
+
+  return <>
+    <div className="form-container">
+  <form id='addRecipe-form' onSubmit={handleSubmit}>
+
+    <label>Recipe name</label>
+    <input type="text" onChange ={handleName}
+     value={recipeName} id="recipeName" placeholder="Recipe name.."/>
+
+    <label>Image URL</label>
+    <input type="text"  onChange ={handleUrl}
+    value={imgUrl} id="imageUrl" placeholder="Image URL here"/>
+
+
+    <select value={cuisine} onChange={handleCuisine}>
+      <option>Select cuisine</option>
+      <option>American</option>
+      <option>Global</option>
+      <option>Cuisine</option>
+      <option>Italian</option>
+      <option>Vegetarian</option>
+      <option>Asian</option>
+      <option>Mexican</option>
+    </select>
+
+    <label>Steps</label>
+    <textarea id="steps"  onChange ={handleSteps}
+    value={steps} placeholder="Describe the steps of the recipe.." style={{height:"200px"}}></textarea>
+    <div> <input type="submit" value="Submit"/>
+    <Link to="/recipes" className='myButton'>See recipes</Link>
+    </div>
+   
+
+  </form>
+</div> 
+  </>
 }
--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -1,7 +1,117 @@
-import React from 'react'
+import React, {useState, useContext} from 'react'
+import { UserContext } from './user'
 
-export default function RecipeCard() {
-  return (
-    <div>RecipeCard</div>
-  )
+export default function Card({recipe, recipeList, setRecipeList}) {
+  const [btn, setBtn] = useState(true)
+  const { user, setUser } = useContext(UserContext);
+  const [newSteps, setNewSteps] = useState("")
+  const [clickedRecipe, setRecipe] = useState(recipe)
+  //changing state when the btn is clicked
+ 
+  function handleClick(){
+    setBtn(!btn)   
+    console.log(recipe.user.username) 
+    console.log(user.username)
+  }
+
+  function handleDelete(){   
+    console.log(recipeList)
+   
+    fetch(`/recipes/${recipe.id}`,{method: 'DELETE'})
+    .then(r=>{
+      if(r.ok){
+        const newRecipeList = recipeList.filter((r)=>{
+          return r.id!= recipe.id          
+        })
+        setRecipeList(newRecipeList)  
+        
+
+      }
+      })
+    
+    }
+    function handleChange(e){
+        setNewSteps(e.target.value)
+
+    }
+    function handleEdit(e){
+        const clickedReview = recipeList.find((recipe)=>{return recipe.user.username === user.username})
+        e.preventDefault()
+        fetch(`/recipes/${recipe.id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({steps: newSteps}
+          ),
+        }).then((r) => r.json())
+        .then((newRecipe)=>{
+              console.log(newRecipe)
+              const newList = recipeList.map((recipe)=>{
+            if(recipe.id === newRecipe.id){
+              return newRecipe
+            }
+            else {
+              return recipe
+            }
+          })
+         setRecipeList(newList)
+         setNewSteps("")
+       
+        })    
+      }  
+
+      function addToFavorites(){
+        
+       fetch(`/favorites`,{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({recipe_name: recipe.name,
+        recipe_steps: recipe.steps, image_url: recipe.image_url, user_id: user.id, cuisine_id: recipe.cuisine_id})
+      })
+      .then(r=>r.json())
+      .then((fav)=>{
+        console.log(fav) 
+      })
+      }
+    
+
+
+  return <>
+  <div className='card' style={{backgroundImage: `url(${recipe.image_url})`}}>
+  
+        <div className="overlay">
+            
+            <h4>{recipe.name}</h4>
+            <button id="favorite" onClick={addToFavorites}>Add to Favorites</button> 
+            <div className={!btn? "overlay2": "steps"}> 
+              <p><small>{recipe.steps}</small></p>
+              {user&&user.username===recipe.user.username?
+               <form onSubmit={handleEdit} className={!btn? "overlay2": "steps"}>
+               <textarea value={newSteps} onChange={handleChange}>
+
+               </textarea>
+               <input type="submit" value="Edit"/>
+             </form>:null}
+             
+              
+            </div>
+
+                 
+            
+        </div>
+        
+     <div id='card-bottom'>
+     {user&&user.username===recipe.user.username?<button id="del-btn"onClick={handleDelete}>Delete</button>:null} 
+     
+    <button className="card-btn" onClick={handleClick} style={{background: !btn? "rgba(0, 0, 0, 0.5)": "#000"}}>{!btn?"Hide Recipe": "Show Recipe"}</button> 
+
+
+    </div>   
+    
+  </div>
+  
+  </>
 }
